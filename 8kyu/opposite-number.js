fb4a4299583f41082c0f3974d95304cdeb5410b0
Evhen87 <kushner.aleks@yandex.ru>
2025-04-13 23:03:39 +0300

    Opposite number

---
+++ 8kyu/opposite-number.js
@@ -0,0 +1,10 @@
+//Task
+/*Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+Examples:
+
+1: -1
+14: -14
+-34: 34*/
+//Solution
+let opposite = number => -(number);
