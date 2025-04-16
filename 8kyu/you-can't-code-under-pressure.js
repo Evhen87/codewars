f963eec7740d8f21dd6cea6d536f70f4f63237b6
Evhen87 <kushner.aleks@yandex.ru>
2025-04-17 00:10:27 +0300

    double the integer

---
+++ 8kyu/you-can't-code-under-pressure.js
@@ -0,0 +1,10 @@
+//Task
+/*Code as fast as you can! You need to double the integer and return it.*/
+
+function doubleInteger(i) {
+   // i will be an integer. Double it and return it.
+   i = i * 2;
+   return i;
+}
+//const doubleInteger = i => i = i * 2;
+//Best practices
