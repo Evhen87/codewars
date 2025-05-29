3de08e84d8f9d874e4478bf4435a7d370550dff8
Evhen87 <kushner.aleks@yandex.ru>
2025-05-29 18:02:19 +0300

    Find the position!

---
+++ 7kyu/find-the-position.js
@@ -0,0 +1,12 @@
+//Task
+// When provided with a letter, return its position in the alphabet.
+//
+// Input :: "a"
+//
+// Output :: "Position of alphabet: 1"
+//
+// Note: Only lowercased English letters are tested
+//Solution
+function position(letter){
+    return `Position of alphabet: ${letter.charCodeAt(0) - 96}`;
+}
