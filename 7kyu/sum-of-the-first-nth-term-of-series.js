7574aab3a4fd076fc1d5ff32fc6bd743138648c5
Evhen87 <kushner.aleks@yandex.ru>
2025-06-22 11:46:27 +0300

    Sum of the first nth term of Series 7 kyu

---
+++ 7kyu/sum-of-the-first-nth-term-of-series.js
@@ -0,0 +1,13 @@
+//Task
+// Your task is to write a function which returns the n-th term of the following series, which is the sum of the first n terms of the sequence (n is the input parameter).
+
+//Solution
+
+function SeriesSum(n) {
+    // Happy Coding ^_^
+    let sum = 0
+    for (let i = 0; i < n; i++) {
+        sum += 1/(1+3*i)
+    }
+    return sum.toFixed(2)
+}
