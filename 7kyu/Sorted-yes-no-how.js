bdf348f7384e4888d9e689a461fd1da97a59458a
Evhen87 <kushner.aleks@yandex.ru>
2025-07-10 11:47:38 +0300

    Sorted? yes? no? how? 7 kyu

---
+++ 7kyu/Sorted-yes-no-how.js
@@ -0,0 +1,32 @@
+//Task
+// Complete the method which accepts an array of integers, and returns one of the following:
+//
+//     "yes, ascending" - if the numbers in the array are sorted in an ascending order
+// "yes, descending" - if the numbers in the array are sorted in a descending order
+// "no" - otherwise
+// The order does not have to be strict: a sorted array can contain consecutive duplicates, e.g. [1, 1, 2, 3] is sorted in ascending order.
+//
+//     It is guaranteed that there will always be a unique valid answer. More precisely:
+//
+//     there will be no arrays with less than 2 elements
+// there will be no arrays where all elements are equal
+
+//Solution
+
+function isSortedAndHow(array) {
+    let sortedArrayAsc = [...array].sort((a, b) => a - b)
+    let sortedArrayDesc = [...array].sort((a, b) => b - a)
+    if (array.toString() === sortedArrayAsc.toString()) {
+        return "yes, ascending"
+    } if (array.toString() === sortedArrayDesc.toString()) {
+        return "yes, descending"
+    } else {
+        return "no"
+    }
+}
+//Best practice
+// function isSortedAndHow(array) {
+//     if([...array].sort((a,b)=>a-b).join`` === array.join``) return "yes, ascending";
+//     if([...array].sort((a,b)=>b-a).join`` === array.join``) return "yes, descending";
+//     return "no";
+// }
