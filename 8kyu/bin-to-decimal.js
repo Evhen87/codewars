a278be6acaf3df3ffe11d06ff22fe03291e11907
Evhen87 <kushner.aleks@yandex.ru>
2025-07-26 17:08:54 +0300

    Bin to Decimal 8 kyu

---
+++ 8kyu/bin-to-decimal.js
@@ -0,0 +1,12 @@
+//Task
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+//Solution
+
+// function binToDec(bin) {
+//     return parseInt(bin, 2)
+// }
+
+//Solution 2
+
+const binToDec = bin => parseInt(bin, 2)
