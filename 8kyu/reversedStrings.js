6c6be4e22facd82c060e387144316c24fd5d9ad9
Evhen87 <kushner.aleks@yandex.ru>
2025-09-01 23:12:58 +0300

    Reversed Strings 8 kyu

---
+++ 8kyu/reversedStrings.js
@@ -0,0 +1,16 @@
+//Task
+
+// Complete the solution so that it reverses the string passed into it.
+//
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+//Solution
+
+// function solution(str){
+//    return  str.split('').reverse().join('')
+// }
+
+//Solution 2
+
+const solution = (str) => str.split('').reverse().join('')
