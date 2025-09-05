c1da9a869fc11250a2d7e5a3035d1b7ef9055296
Evhen87 <kushner.aleks@yandex.ru>
2025-09-05 20:54:09 +0300

    Switcheroo 7 kyu

---
+++ 7kyu/switcheroo.js
@@ -0,0 +1,19 @@
+//Task
+
+// Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+//
+//     Example:
+//
+//     'acb' --> 'bca'
+//     'aabacbaa' --> 'bbabcabb'
+
+//Solution
+
+// function switcheroo(x){
+//     return x.split('').map(el => el === 'a' ? el = 'b' : el === 'b' ? el = 'a' : el).join('')
+// }
+
+//Solution 2
+
+const switcheroo = (x) => x.split('')
+    .map(el => el === 'a' ? el = 'b' : el === 'b' ? el = 'a' : el).join('')
