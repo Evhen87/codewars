f214e7f4a71b246e35a93db0e1cf6a87e0f73570
Evhen87 <kushner.aleks@yandex.ru>
2025-09-19 10:12:43 +0300

    Kata Example Twist 8 kyu

---
+++ 8kyu/kataExampleTwist.js
@@ -0,0 +1,13 @@
+//Task
+
+// This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+//
+// Add the value "codewars" to the array websites 1,000 times.
+
+//Solution
+
+let websites = []
+
+for (let i = 0; websites.length < 1000 ; i++) {
+    websites[i] = "codewars"
+}
