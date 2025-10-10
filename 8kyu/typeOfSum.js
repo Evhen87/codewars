c278ebf58d9007088a43cfbbeb9a9e1d0427df6b
Evhen87 <kushner.aleks@yandex.ru>
2025-10-10 22:22:09 +0300

    Type of sum 8 kyu

---
+++ 8kyu/typeOfSum.js
@@ -0,0 +1,13 @@
+//Task
+
+// Return the type of the sum of the two arguments
+
+//Solution
+
+// function typeOfSum(a, b) {
+//     return typeof (a + b)
+// }
+
+//Solution 2
+
+const typeOfSum = (a, b) => typeof (a + b)
