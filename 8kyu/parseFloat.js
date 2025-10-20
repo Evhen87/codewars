f7728515b402df79da531b1ec9bf0cf25b3e4a3d
Evhen87 <kushner.aleks@yandex.ru>
2025-10-20 22:59:27 +0300

    Parse float * kyu

---
+++ 8kyu/parseFloat.js
@@ -0,0 +1,7 @@
+//Task
+
+// Write function parseF which takes an input and returns a number or null if conversion is not possible. The input can be one of many different types so be aware.
+
+//Solution
+
+const parseF = (s) => isFinite(parseFloat(s)) ? parseFloat(s) : null
