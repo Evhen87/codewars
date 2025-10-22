e91030cf46da318e60476bb77736a7cc84bd9d48
Evhen87 <kushner.aleks@yandex.ru>
2025-10-22 12:42:43 +0300

    Simple validation of a username with regex 8 kyu

---
+++ 8kyu/simpleValidationOfAUsernameWithRegex.js
@@ -0,0 +1,20 @@
+//Task
+
+// Write a simple regex to validate a username. Allowed characters are:
+//
+//     lowercase letters,
+//     numbers,
+//     underscore
+// Length should be between 4 and 16 characters (both included).
+
+//Solution
+
+// function validateUsr(username) {
+//     const res =  /^[a-z0-9_]{4,16}$/.test(username)
+//     return res;
+// }
+
+//Solution 2
+
+const validateUsr = (username) =>  /^[a-z0-9_]{4,16}$/.test(username)
+
