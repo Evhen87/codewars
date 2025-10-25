2bbb2df63e89c4ebffe20c3666a93619a74bfa59
Evhen87 <kushner.aleks@yandex.ru>
2025-10-25 12:04:57 +0300

    Two Sum 6 kyu

---
+++ 6kyu/twoSum.js
@@ -0,0 +1,26 @@
+//Task
+
+// Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indexes of these items should then be returned in a tuple / list (depending on your language) like so: (index1, index2).
+//
+//     For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
+//
+//     The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; target will always be the sum of two different items from that array).
+
+//Solution
+
+function twoSum(numbers, target) {
+    let arr = []
+    if (arr.length < 2) {
+        for (let i = 0; i < numbers.length; i++) {
+            for (let j = 0; j < numbers.length; j++) {
+                if (i !== j) {
+                    if (numbers[i] + numbers[j] === target) {
+                        arr.push(i)
+                        arr.push(j)
+                        return arr
+                    }
+                }
+            }
+        }
+    }
+}
