7803fed1a4aa1209d83686475d13b9439c5bcfb4
Evhen87 <kushner.aleks@yandex.ru>
2025-12-27 23:50:15 +0300

    NBA full 48 minutes average 8 kyu

---
+++ 8kyu/NBAFull48MinutesAverage.js
@@ -0,0 +1,15 @@
+//Task
+
+// An NBA game runs 48 minutes (Four 12 minute quarters). Players do not typically play the full game, subbing in and out as necessary. Your job is to extrapolate a player's points per game if they played the full 48 minutes.
+//
+// Write a function that takes two arguments, ppg (points per game) and mpg (minutes per game) and returns a straight extrapolation of ppg per 48 minutes rounded to the nearest tenth. Return 0 if 0.
+
+//Solution
+
+// function pointsPer48(ppg, mpg) {
+//     return Math.round(ppg / mpg * 48 * 10) / 10 || 0
+// }
+
+//Solution 2
+
+const pointsPer48 = (ppg, mpg) => Math.round(ppg / mpg * 48 * 10) / 10 || 0
