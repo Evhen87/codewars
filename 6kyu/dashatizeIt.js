e09f2e6fd752abb1caea1ab3e205430bc90fa7b5
Evhen87 <kushner.aleks@yandex.ru>
2025-12-29 22:39:57 +0300

    Dashatize it 6 kyu

---
+++ 6kyu/dashatizeIt.js
@@ -0,0 +1,21 @@
+//Task
+
+// Given an integer, return a string with dash '-' marks before and after each odd digit, but do not begin or end the string with a dash mark.
+//
+//     Ex:
+//
+// 274 -> '2-7-4'
+// 6815 -> '68-1-5'
+
+//Solution
+
+function dashatize(num) {
+    return Math.abs(num)
+        .toString().split('')
+        .reduce((acc, el, ind, array) => ind === array.length - 1 ? acc = acc + `${el}` :
+                +array[ind + 1] % 2 === 0 && +el % 2 !== 0 ? acc = acc + `${el}-` :
+                    +array[ind + 1] % 2 !== 0 && +el % 2 === 0 ? acc = acc + `${el}-` :
+                        +array[ind + 1] % 2 !== 0 && +el % 2 !== 0 ? acc = acc + `${el}-` :
+                            acc = acc + el
+            , '')
+}
