066e6f08699e6f5f1c58a67a2e829810d6778c3d
Evhen87 <kushner.aleks@yandex.ru>
2026-01-03 23:54:44 +0300

    Squares sequence 7 kyu

---
+++ 7kyu/squaresSequence.js
@@ -0,0 +1,20 @@
+//Task
+
+// Complete the function that returns an array of length n, starting with the given number x and the squares of the previous number. If n is negative or zero, return an empty array/list.
+//
+//     Examples
+// 2, 5  -->  [2, 4, 16, 256, 65536]
+// 3, 3  -->  [3, 9, 81]
+
+//Solution
+
+function squares(x, n) {
+    const arr = []
+    let num = x
+
+    for (let i = 0; i < n; i++ ) {
+        arr.push(num)
+        num = num ** 2
+    }
+    return arr
+}
