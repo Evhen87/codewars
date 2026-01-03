55e96ea9de12114c1f32062565dcebe06540c971
Evhen87 <kushner.aleks@yandex.ru>
2026-01-03 22:11:57 +0300

    The Old Switcheroo 7 kyu

---
+++ 7kyu/theOldSwitcheroo.js
@@ -0,0 +1,26 @@
+//Task
+
+// Write a function
+//
+// vowel2index(str)
+// that takes in a string and replaces all the vowels [a,e,i,o,u] with their respective positions within that string.
+//     E.g:
+//
+// vowel2index('this is my string') == 'th3s 6s my str15ng'
+// vowel2index('Codewars is the best site in the world') == 'C2d4w6rs 10s th15 b18st s23t25 27n th32 w35rld'
+// vowel2index('') == ''
+
+//Solution
+
+function vowel2index(str) {
+    const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+
+    const arr = str.split('')
+
+    for (let i = 0; i < arr.length; i++) {
+        if (vowels.includes(arr[i])) {
+            arr[i] = i + 1
+        }
+    }
+    return arr.join('')
+}
