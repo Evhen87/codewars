6e4a414227305a20dfa5ed7234f51595e454060f
Evhen87 <kushner.aleks@yandex.ru>
2026-01-07 23:25:59 +0300

    Find min and max 7 kyu

---
+++ 7kyu/findMinAndMax.js
@@ -0,0 +1,13 @@
+//Task
+
+// Implement a function that returns the minimal and the maximal value of a list (in this order).
+
+//Solution
+
+// function getMinMax(arr){
+//     return [Math.min(...arr), Math.max(...arr)]
+// }
+
+//Solution 2
+
+const  getMinMax = (arr) => [Math.min(...arr), Math.max(...arr)]
