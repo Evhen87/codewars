efddc37dee78566dfd923217a5dcb07e94736fc4
Evhen87 <kushner.aleks@yandex.ru>
2026-01-08 19:50:30 +0300

    All Star Code Challenge #22 7 kyu

---
+++ 7kyu/allStarCodeChallenge.js
@@ -0,0 +1,32 @@
+//Task
+
+// This Kata is intended as a small challenge for my students
+//
+// All Star Code Challenge #22
+//
+// Create a function that takes an integer argument of seconds and converts the value into a string describing how many hours and minutes comprise that many seconds.
+//
+//     Any remaining seconds left over are ignored.
+//
+//     Note:
+// The string output needs to be in the specific form - "X hour(s) and X minute(s)"
+//
+// For example:
+//
+//     3600 --> "1 hour(s) and 0 minute(s)"
+// 3601 --> "1 hour(s) and 0 minute(s)"
+// 3500 --> "0 hour(s) and 58 minute(s)"
+// 323500 --> "89 hour(s) and 51 minute(s)"
+
+//Solution
+
+// function toTime(seconds) {
+//     const hour = Math.floor(seconds / 3600)
+//     const minute = Math.floor((seconds - hour * 3600) / 60)
+//
+//     return `${hour} hour(s) and ${minute} minute(s)`
+// }
+
+//Solution 2
+
+const toTime = (seconds) =>  `${Math.floor(seconds / 3600)} hour(s) and ${Math.floor((seconds % 3600) / 60)} minute(s)`
