e1e2a0ad7096c38f3018eeb7b6649de66bd47243
Evhen87 <kushner.aleks@yandex.ru>
2026-01-11 22:13:30 +0300

    Basic Math (Add or Subtract) 7 kyu

---
+++ 7kyu/basicMathAddOrSubtract.js
@@ -0,0 +1,19 @@
+//Task
+
+// In this kata, you will do addition and subtraction on a given string. The return value must be also a string.
+//
+//     Note: the input will not be empty.
+//
+//     Examples
+// "1plus2plus3plus4"  --> "10"
+// "1plus2plus3minus4" -->  "2"
+
+//Solution
+
+// function calculate(str) {
+//     return eval(str.replace(/plus/g, '+').replace(/minus/g, '-')).toString();
+// }
+
+//Solution 2
+
+const calculate = (str) => eval(str.replace(/plus/g, '+').replace(/minus/g, '-')).toString()
