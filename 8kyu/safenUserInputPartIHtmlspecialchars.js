6f1104ad1be6ce3f5c3836304b694c6bc0288a53
Evhen87 <kushner.aleks@yandex.ru>
2026-01-11 19:41:56 +0300

    Safen User Input Part I - htmlspecialchars 8 kyu

---
+++ 8kyu/safenUserInputPartIHtmlspecialchars.js
@@ -0,0 +1,45 @@
+//Task
+
+// Safen User Input Part I - htmlspecialchars
+// You are a(n) novice/average/experienced/professional/world-famous Web Developer (choose one) who owns a(n) simple/clean/slick/beautiful/complicated/professional/business website (choose one or more) which contains form fields so visitors can send emails or leave a comment on your website with ease. However, with ease comes danger. Every now and then, a hacker visits your website and attempts to compromise it through the use of XSS (Cross Site Scripting). This is done by injecting script tags into the website through form fields which may contain malicious code (e.g. a redirection to a malicious website that steals personal information).
+//
+// Mission
+// Your mission is to implement a function that converts the following potentially harmful characters:
+//
+//     < --> &lt;
+// > --> &gt;
+// " --> &quot;
+// & --> &amp;
+// Good luck :D
+
+//Solution
+
+function htmlspecialchars(formData) {
+    const htmlSpecialChars = {
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        '&': '&amp;'
+    }
+
+    let arr = formData.split('')
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] in htmlSpecialChars) {
+            arr[i] = htmlSpecialChars[arr[i]]
+        }
+
+    }
+    arr = arr.join('')
+
+    return arr
+}
+
+//Best practice
+
+// function htmlspecialchars(formData) {
+//     return formData.replace(/&/g, "&amp;")
+//         .replace(/"/g, "&quot;")
+//         .replace(/</g, "&lt;")
+//         .replace(/>/g, "&gt;");
+// }
