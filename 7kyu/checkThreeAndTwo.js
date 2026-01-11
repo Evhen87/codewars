96e37857e2e77b24fd541ae3bbdfc062b8ede2ad
Evhen87 <kushner.aleks@yandex.ru>
2026-01-11 20:18:13 +0300

    Check three and two 8 kyu

---
+++ 7kyu/checkThreeAndTwo.js
@@ -0,0 +1,25 @@
+//Task
+
+// Given an array with exactly 5 strings "a", "b" or "c" (chars in Java, characters in Fortran, Chars in Haskell), check if the array contains three and two of the same values.
+//
+//     Examples
+//     ["a", "a", "a", "b", "b"] ==> true  // 3x "a" and 2x "b"
+//     ["a", "b", "c", "b", "c"] ==> false // 1x "a", 2x "b" and 2x "c"
+//     ["a", "a", "a", "a", "a"] ==> false // 5x "a"
+
+//Solution
+
+function checkThreeAndTwo(array) {
+    const obj = {}
+
+    for (let i = 0; i < array.length; i++) {
+        if (!(array[i] in obj)) {
+            obj[array[i]] = 1
+        } else {
+            obj[array[i]]++
+        }
+    }
+    const values = Object.values(obj)
+
+    return values.includes(3) && values.includes(2)
+}
