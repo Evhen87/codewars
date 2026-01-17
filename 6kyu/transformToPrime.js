a0a599701ce562ebbbbcafa1c387c825d8a71c40
Evhen87 <kushner.aleks@yandex.ru>
2026-01-17 19:44:46 +0300

    Transform To Prime 6 kyu

--- 6kyu/transformToPrime.js
+++ 6kyu/transformToPrime.js
@@ -47,3 +47,20 @@ function minimumNumber(numbers){
     }
 
 }
+
+//Best practice
+
+// function minimumNumber(numbers){
+//     let sum = numbers.reduce((a,b) => a + b)
+//     for(let i = sum; ; i++) {
+//         if(prime(i)) return i - sum
+//     }
+//     function prime(a) {
+//         if(a < 2) return false
+//         if(a % 2 === 0) return a === 2
+//         for(let i = 3; i * i < a; i += 2) {
+//             if(a % i === 0) return false
+//         }
+//         return true
+//     }
+// }
