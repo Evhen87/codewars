a1b92e1381282e00791e0d1c8484a83ab907fc0f
Evhen87 <kushner.aleks@yandex.ru>
2026-01-24 10:55:48 +0300

    Exclamation marks series #17: Put the exclamation marks and question marks on the balance - are they balanced? 6 kyu

---
+++ 6kyu/areTheyBalanced.js
@@ -0,0 +1,20 @@
+//Task
+
+// Each exclamation mark's weight is 2; each question mark's weight is 3. Putting two strings left and right on the balance - are they balanced?
+//
+//     If the left side is more heavy, return "Left"; if the right side is more heavy, return "Right"; if they are balanced, return "Balance".
+//
+//     Examples
+// "!!", "??"     -->  "Right"
+// "!??", "?!!"   -->  "Left"
+// "!?!!", "?!?"  -->  "Left"
+// "!!???!????", "??!!?!!!!!!!"  -->  "Balance"
+
+//Solution
+
+function balance(left, right) {
+    function weight(side) {
+        return side.split('').reduce((acc, el) => el === '!' ? acc + 2 : acc + 3 , 0)
+    }
+    return weight(left) > weight(right) ? "Left" : weight(left) < weight(right) ? "Right" : "Balance";
+}
