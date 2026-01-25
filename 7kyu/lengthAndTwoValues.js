0b90e7fb777221bbd28995c415258b4d9413a214
Evhen87 <kushner.aleks@yandex.ru>
2026-01-25 23:40:23 +0300

    Length and two values. 7 kyu

---
+++ 7kyu/lengthAndTwoValues.js
@@ -0,0 +1,23 @@
+//Task
+
+// Given an integer n and two other values, build an array of size n filled with these two values alternating.
+//
+//     Examples
+// 5, true, false     -->  [true, false, true, false, true]
+// 10, "blue", "red"  -->  ["blue", "red", "blue", "red", "blue", "red", "blue", "red", "blue", "red"]
+// 0, "one", "two"    -->  []
+
+//Solution
+
+function alternate(n, firstValue, secondValue){
+    const arr = []
+    for (let i = 0; i < n; i += 2) {
+        arr[i] = firstValue
+        if (i + 1 < n) {
+            arr[i + 1] = secondValue
+        }
+    }
+    return arr
+}
+
+//
