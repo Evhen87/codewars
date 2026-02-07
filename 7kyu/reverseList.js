0a55ddb985412502f15b9f47c47b9e8d399064bc
Evhen87 <kushner.aleks@yandex.ru>
2026-02-07 20:28:36 +0300

    Reverse List 7 kyu

---
+++ 7kyu/reverseList.js
@@ -0,0 +1,14 @@
+//Task
+
+// Write reverseList function that simply reverses lists.
+
+//Solution
+
+// function reverseList(arr) {
+//     return arr.reverse();
+// }
+
+//Solution
+
+const reverseList = (arr) => arr.reverse()
+
