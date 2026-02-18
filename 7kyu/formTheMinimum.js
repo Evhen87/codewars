0daecb82bdfc0bb23e59b34fb5769b094c16e0d2
Evhen87 <kushner.aleks@yandex.ru>
2026-02-18 22:15:53 +0300

    Form The Minimum 7 kyu

---
+++ 7kyu/formTheMinimum.js
@@ -0,0 +1,21 @@
+//Task
+
+// Task
+// Given a list of digits, return the smallest number that could be formed from these digits, using the digits only once (ignore duplicates). Only positive integers in the range of 1 to 9 will be passed to the function.
+//
+// Examples
+//     [1, 3, 1] ==> 13
+//     [5, 7, 5, 9, 7] ==> 579
+//     [1, 9, 3, 1, 7, 4, 6, 6, 7]  ==> 134679
+
+//Solution
+
+function minValue(values){
+    let arr = []
+    for (let i = 0; i < values.length; i++ ) {
+        if (!arr.includes(values[i])) {
+            arr.push(values[i])
+        }
+    }
+    return +arr.sort().join('')
+}
