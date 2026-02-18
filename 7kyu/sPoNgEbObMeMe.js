cf6ed8ad4293db1420490ed352b93132d7cdb27a
Evhen87 <kushner.aleks@yandex.ru>
2026-02-18 21:51:57 +0300

    sPoNgEbOb MeMe 7 kyu

---
+++ 7kyu/sPoNgEbObMeMe.js
@@ -0,0 +1,22 @@
+//Task
+
+// Remember the spongebob meme that is meant to make fun of people by repeating what they say in a mocking way?
+//
+//     "Dont use that weird spongebob mocking meme" Me: DonT uSe thAt WeIrd SpoNgEboB MoCkinG MEme
+//
+// You need to create a function that converts the input into this format, with the output being the same string expect there is a pattern of uppercase and lowercase letters.
+//
+//     Example:
+//
+// input:  "stop Making spongebob Memes!"
+// output: "StOp mAkInG SpOnGeBoB MeMeS!"
+
+//Solution
+
+// function spongeMeme(sentence) {
+//     return sentence.split('').map((char, i) => i % 2 === 0 ? char.toUpperCase() : char.toLowerCase()).join('')
+// }
+
+//Solution 2
+
+const spongeMeme = (sentence) => sentence.split('').map((char, i) => i % 2 === 0 ? char.toUpperCase() : char.toLowerCase()).join('')
