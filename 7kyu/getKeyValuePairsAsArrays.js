57980cb545256f594eb5f14ef66c19456ac4b976
Evhen87 <kushner.aleks@yandex.ru>
2026-03-01 20:57:39 +0300

    Get key/value pairs as arrays 7 kyu

---
+++ 7kyu/getKeyValuePairsAsArrays.js
@@ -0,0 +1,20 @@
+//Task
+
+// Complete the keysAndValues function so that it takes in an object and returns the keys and values as separate arrays.
+//
+//     Example:
+//
+// keysAndValues({a: 1, b: 2, c: 3}) // should return [['a', 'b', 'c'], [1, 2, 3]]
+// Style Points (JS/CoffeeScript only): This kata only tests for data that uses object literal notation (simple objects). For extra style, can you get your method to check for objects that extend their prototype?
+
+//Solution
+
+function keysAndValues(data){
+    const keys = []
+    const values = []
+    for (let key in data) {
+        keys.push(key)
+        values.push(data[key])
+    }
+    return [keys, values]
+}
