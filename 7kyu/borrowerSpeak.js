d4ba26c6fafb4e7b0707a801bca385ac134c56f1
Evhen87 <kushner.aleks@yandex.ru>
2026-03-10 23:03:49 +0300

    Borrower Speak 7 kyu

---
+++ 7kyu/borrowerSpeak.js
@@ -0,0 +1,22 @@
+//Task
+
+// The borrowers are tiny tiny fictional people. As tiny tiny people they have to be sure they aren't spotted, or more importantly, stepped on.
+//
+// As a result, the borrowers talk very very quietly. They find that capitals and punctuation of any sort lead them to raise their voices and put them in danger.
+//
+//     The young borrowers have begged their parents to stop using caps and punctuation.
+//
+//     Change the input text s to new borrower speak. Help save the next generation!
+
+//Solution
+
+function borrow(s){
+    const arr = s.split('')
+    const resArr = []
+    for (let char of arr) {
+        if (char.toUpperCase() !== char.toLowerCase()) {
+            resArr.push(char.toLowerCase())
+        }
+    }
+    return resArr.join('')
+}
