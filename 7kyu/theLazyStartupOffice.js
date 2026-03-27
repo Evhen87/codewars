b5622e27e8cbf3e3e5a21f0365042793d12a2400
Evhen87 <kushner.aleks@yandex.ru>
2026-03-27 20:50:13 +0300

    The Lazy Startup Office 7 kyu

---
+++ 7kyu/theLazyStartupOffice.js
@@ -0,0 +1,28 @@
+//Task
+
+// A startup office has an ongoing problem with its bin. Due to low budgets, they don't hire cleaners. As a result, the staff are left to voluntarily empty the bin. It has emerged that a voluntary system is not working and the bin is often overflowing. One staff member has suggested creating a rota system based upon the staff seating plan.
+//
+// Create a function that accepts a 2D array of names. The function will return a single array containing staff names in the order that they should empty the bin.
+//
+//     Adding to the problem, the office has some temporary staff. This means that the seating plan changes every month. Both staff members' names and the number of rows of seats may change. Ensure that the function works when tested with these changes.
+//
+// Notes:
+//
+//     All the rows will always be the same length as each other.
+//     There will be no empty spaces in the seating plan.
+//     There will be no empty arrays.
+//     Each row will be at least one seat long.
+
+//Solution
+
+function binRota(arr){
+    let res = []
+    for (let i = 0; i < arr.length; i++) {
+        let currentArr = arr[i]
+        if (i % 2 !== 0) {
+            currentArr = currentArr.reverse()
+        }
+        res = res.concat(currentArr)
+    }
+    return res
+}
