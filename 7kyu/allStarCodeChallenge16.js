1f42152f7f25b6a311a713b90ec42e199d0438c0
Evhen87 <kushner.aleks@yandex.ru>
2026-03-29 17:00:56 +0300

    All Star Code Challenge #16 7 kyu

---
+++ 7kyu/allStarCodeChallenge16.js
@@ -0,0 +1,22 @@
+//Task
+
+// This Kata is intended as a small challenge for my students
+//
+// All Star Code Challenge #16
+//
+// Create a function called noRepeat() that takes a string argument and returns a single letter string of the first not repeated character in the entire string.
+//
+// noRepeat("aabbccdde") // => "e"
+// noRepeat("wxyz") // => "w"
+// noRepeat("testing") // => "e"
+// Note: ONLY letters from the english alphabet will be used as input There will ALWAYS be at least one non-repeating letter in the input string
+
+//Solution
+
+function noRepeat(str) {
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] !== str[i + 1] &&
+            str[i] !== str[i - 1] &&
+            str.split('').filter(el => el === str[i]).length === 1 ) return str[i]
+    }
+}
